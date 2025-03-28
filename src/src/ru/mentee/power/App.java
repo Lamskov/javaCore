365e5603c8cae34bb30ccc3d10358992e7544160
Lamskov <dima.lamskoff@yandex.ru>
2025-03-28 10:20:00 +0300

    New project at Java

---
+++ src/src/ru/mentee/power/App.java
@@ -0,0 +1,9 @@
+package ru.mentee.power;
+
+public class App {
+    public static void main(String[] args) {
+        int myAge = 25;
+        String myName = "Dmitriy";
+        System.out.println("My name is: " + myName + "\nI'm " + myAge + " years old.");
+    }
+}
